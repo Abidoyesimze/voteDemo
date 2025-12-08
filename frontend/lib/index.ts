f0a5d777647bec27c499a287f25793f986ab47aa
Abidoyesimze <similoluwaeyitayoabidoye@gmail.com>
2025-12-08 04:53:48 +0100

    Add barrel exports for easier imports across the codebase

---
+++ frontend/lib/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Library Utilities Barrel Export
+ */
+export * from './constants';
+export * from './utils';
+export * from './validation';
+export * from './contract';
+export * from './env';
+export * from './types';
+export * from './transactionTracker';
+export * from './share';
+export * from './export';
+export * from './errorMessages';
+export * from './accessibility';
+export * from './responsive';
+export * from './analytics';
+export * from './performance';
+export * from './storage';
+
